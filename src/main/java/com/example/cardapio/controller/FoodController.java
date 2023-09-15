417a4b3304fcd6d1cb19f6f3a23b91d54b8de8ff
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2023-09-15 17:51:13 -0300

    r

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,12 +1,15 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.CardapioApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
-    public void getAll(){
+    public void main(String[] args){
+        SpringApplication.run(CardapioApplication.class, args);
 
     }
 }
